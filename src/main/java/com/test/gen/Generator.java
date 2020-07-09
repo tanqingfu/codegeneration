c1314b0d3153a82cb39715d5b5a9f354a51bf1d4
tanqingfu <tqf18290544146@sohu.com>
2020-07-09 17:05:46 +0800

    代码生成器  根据数据库表生成 mapper service controller model

---
+++ src/main/java/com/test/gen/Generator.java
@@ -0,0 +1,53 @@
+package com.test.gen;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+
+/**
+ * Created by zzq on 2018/5/11.
+ */
+public class Generator {
+
+    // 数据库连接
+ /*   private DbType dbType = DbType.ORACLE;
+    private String driverName = "oracle.jdbc.driver.OracleDriver";
+    private String dbUrl = "jdbc:oracle:thin:@192.168.1.132:1521:ORCL";
+    private String dbName = "TEXT";
+    private String dbPwd = "123456";*/
+    
+    private DbType dbType = DbType.MYSQL;
+    private String driverName = "com.mysql.cj.jdbc.Driver";
+    private String dbUrl = "jdbc:mysql://localhost:3306/xinjue?serverTimezone=Asia/Shanghai&useSSL=false&useUnicode=true&characterEncoding=UTF-8";
+    private String dbName = "root";
+    private String dbPwd = "root";
+    
+    // 作者
+    private String author = "tqf";
+    // 包信息
+    private String projectName = "xinjue-test";
+    private String packageName = "com.xinjue-test";
+    private String vuePath = "./static-vue";
+    private Boolean restStyle = true;
+    private String ide = "idea"; // idea eclipse
+
+    public static void main(String[] args) {
+        new Generator().generateCode("users");
+    }
+    
+    public void generateCode(String... tables) {
+        new S().generateByTables(
+                new GenConfig()
+                        .setDbType(dbType)
+                        .setDriverName(driverName)
+                        .setAuthor(author)
+                        .setDbName(dbName)
+                        .setDbPwd(dbPwd)
+                        .setDbUrl(dbUrl)
+                        .setPackageName(packageName)
+                        .setProjectName(projectName)
+                        .setVuePath(vuePath)
+                        .setRestStyle(restStyle)
+                        .setIde(ide),
+                tables);
+    }
+
+}
